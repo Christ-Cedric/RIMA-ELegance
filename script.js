f1396033d38c413bd2dbbff61835fc7ab917a4c6
Christ-Cedric <yameogoclaver39@gmail.com>
2025-09-18 17:01:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ offre.forEach(button => {
     alert('Offre non disponible');
   });
 });
+const buttons = document.querySelectorAll('#produits button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const productName = button.dataset.product; // ajoute data-product sur chaque bouton
+    const productImg = button.dataset.img;     // ajoute data-img sur chaque bouton
+    const waLink = `https://wa.me/54515907?text=Je%20suis%20int%C3%A9ress%C3%A9%20par%20le%20produit%20${encodeURIComponent(productName)}%20${encodeURIComponent(productImg)}`;
+    window.open(waLink, '_blank');
+  });
+});
+
+
